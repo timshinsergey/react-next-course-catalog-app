a41b469718e904722276b43fb82e8f290c37cab1
Sergey <33506866+timshinsergey@users.noreply.github.com>
2022-01-16 19:51:00 +0400

    add fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,7 +18,19 @@ class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang='ru'>
-        <Head />
+        <Head>
+          <link rel='icon' href='/favicon.ico' />
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link
+            rel='preconnect'
+            href='https://fonts.gstatic.com'
+            crossOrigin='true'
+          />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap'
+            rel='stylesheet'
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
