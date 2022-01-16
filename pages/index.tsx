1e59be5d18872318964cad8801084d008b344256
Sergey <33506866+timshinsergey@users.noreply.github.com>
2022-01-16 20:30:22 +0400

    create Htag component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,9 @@
+import { Htag } from '../components'
+
 export default function Home(): JSX.Element {
-  return <div>some text</div>
+  return (
+    <>
+      <Htag tag='h1'>Текст</Htag>
+    </>
+  )
 }
