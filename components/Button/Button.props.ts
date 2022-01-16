f917d2fc46d4b969febb08c6d02026c67903ddbb
Sergey <33506866+timshinsergey@users.noreply.github.com>
2022-01-16 22:00:18 +0400

    create P component

--- components/Button/Button.props.ts
+++ components/Button/Button.props.ts
@@ -1,4 +1,4 @@
-import { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from 'react'
+import { DetailedHTMLProps, ButtonHTMLAttributes, ReactNode } from 'react'
 
 export interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
 	children: ReactNode
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Htag } from '../components'
+import { Button, Htag, P } from '../components'
 
 export default function Home(): JSX.Element {
   return (
@@ -10,6 +10,12 @@ export default function Home(): JSX.Element {
       <Button appearance='ghost' arrow='right'>
         Кнопка
       </Button>
+      <P size='l'>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae odio
+        veniam placeat eaque nihil. Odio consectetur omnis tempora assumenda
+        laborum! Dicta aliquam nihil incidunt ullam libero at, quis molestias
+        debitis.
+      </P>
     </>
   )
 }
