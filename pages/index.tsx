0c9b2b29d6dfb4a96eb65174199bf1d1a4c3941b
Sergey <33506866+timshinsergey@users.noreply.github.com>
2022-01-17 19:44:02 +0400

    create context and menu component

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,11 +33,6 @@ function Home({ menu }: HomeProps): JSX.Element {
       <Tag color='green'>Веб-разработка</Tag>
       <Tag color='primary'>Веб-разработка</Tag>
       <Rating rating={rating} setRating={setRating} isEditable />
-      <ul>
-        {menu.map((m) => (
-          <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
-        ))}
-      </ul>
     </>
   )
 }
