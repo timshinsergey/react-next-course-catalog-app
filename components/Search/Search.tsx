e0fdbbda6bf8061fe41beda3abbf33d1ab107147
Sergey <33506866+timshinsergey@users.noreply.github.com>
2022-01-21 20:11:04 +0400

    create search component

---
+++ components/Search/Search.tsx
@@ -0,0 +1,49 @@
+import { useState, KeyboardEvent, MouseEvent } from 'react'
+import { useRouter } from 'next/router'
+import cn from 'classnames'
+import { SearchProps } from './Search.props'
+import { Input } from '../Input/Input'
+import { Button } from '../Button/Button'
+import styles from './Search.module.css'
+import GlassIcon from './glass.svg'
+
+export const Search = ({ className, ...props }: SearchProps): JSX.Element => {
+  const [search, setSearch] = useState<string>('')
+  const router = useRouter()
+
+  const goToSearch = (event: MouseEvent<HTMLButtonElement> | KeyboardEvent) => {
+    event.preventDefault()
+    router.push({
+      pathname: '/search',
+      query: {
+        q: search,
+      },
+    })
+  }
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key == 'Enter') {
+      goToSearch(event)
+    }
+  }
+
+  return (
+    <form className={cn(className, styles.search)} {...props} role='search'>
+      <Input
+        className={styles.input}
+        placeholder='Поиск...'
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      <Button
+        appearance='primary'
+        className={styles.button}
+        onClick={goToSearch}
+        aria-label='Искать по сайту'
+      >
+        <GlassIcon />
+      </Button>
+    </form>
+  )
+}
--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,14 +1,15 @@
 import cn from 'classnames'
 import { SidebarProps } from './Sidebar.props'
-import styles from './Sidebar.module.css'
 import { Menu } from '../Menu/Menu'
+import { Search } from '../../components'
+import styles from './Sidebar.module.css'
 import Logo from '../logo.svg'
 
 export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
   return (
     <div className={cn(className, styles.sidebar)} {...props}>
       <Logo />
-      <div>поиск</div>
+      <Search />
       <Menu />
     </div>
   )
