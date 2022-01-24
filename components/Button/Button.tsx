4e4b16328514a66d4234d706065b9ea2d1e1865a
Sergey <33506866+timshinsergey@users.noreply.github.com>
2022-01-24 20:05:33 +0400

    update button component

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,17 +1,19 @@
 import cn from 'classnames'
+import { motion } from 'framer-motion'
 import { ButtonProps } from './Button.props'
 import styles from './Button.module.css'
 import ArrowIcon from './arrow.svg'
 
 export const Button = ({
   appearance,
+  arrow = 'none',
   children,
   className,
-  arrow = 'none',
   ...props
 }: ButtonProps): JSX.Element => {
   return (
-    <button
+    <motion.button
+      whileHover={{ scale: 1.05 }}
       className={cn(styles.button, className, {
         [styles.primary]: appearance == 'primary',
         [styles.ghost]: appearance == 'ghost',
@@ -19,15 +21,15 @@ export const Button = ({
       {...props}
     >
       {children}
-      {arrow !== 'none' && (
+      {arrow != 'none' && (
         <span
           className={cn(styles.arrow, {
-            [styles.down]: arrow === 'down',
+            [styles.down]: arrow == 'down',
           })}
         >
           <ArrowIcon />
         </span>
       )}
-    </button>
+    </motion.button>
   )
 }
