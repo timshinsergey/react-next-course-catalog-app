b1631b87d4e4cd71cfe083830b4479a04c0104b0
Sergey <33506866+timshinsergey@users.noreply.github.com>
2022-01-26 14:14:58 +0400

    rating accessibility

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -1,9 +1,10 @@
 import {
-  useState,
   useEffect,
+  useState,
   KeyboardEvent,
   forwardRef,
   ForwardedRef,
+  useRef,
 } from 'react'
 import cn from 'classnames'
 import { RatingProps } from './Rating.props'
@@ -12,16 +13,37 @@ import StarIcon from './star.svg'
 
 export const Rating = forwardRef(
   (
-    { isEditable = false, rating, setRating, error, ...props }: RatingProps,
+    {
+      isEditable = false,
+      error,
+      rating,
+      setRating,
+      tabIndex,
+      ...props
+    }: RatingProps,
     ref: ForwardedRef<HTMLDivElement>
   ): JSX.Element => {
     const [ratingArray, setRatingArray] = useState<JSX.Element[]>(
       new Array(5).fill(<></>)
     )
+    const ratingArrayRef = useRef<(HTMLSpanElement | null)[]>([])
 
     useEffect(() => {
       constructRating(rating)
-    }, [rating])
+    }, [rating, tabIndex])
+
+    const computeFocus = (r: number, i: number): number => {
+      if (!isEditable) {
+        return -1
+      }
+      if (!rating && i == 0) {
+        return tabIndex ?? 0
+      }
+      if (r == i + 1) {
+        return tabIndex ?? 0
+      }
+      return -1
+    }
 
     const constructRating = (currentRating: number) => {
       const updatedArray = ratingArray.map((r: JSX.Element, i: number) => {
@@ -32,38 +54,58 @@ export const Rating = forwardRef(
               [styles.filled]: i < currentRating,
               [styles.editable]: isEditable,
             })}
-            onMouseEnter={() => changeDisplay(i + 1)}
-            onMouseLeave={() => changeDisplay(rating)}
+            onMouseEnter={() => changeDispay(i + 1)}
+            onMouseLeave={() => changeDispay(rating)}
             onClick={() => onClick(i + 1)}
+            tabIndex={computeFocus(rating, i)}
+            onKeyDown={handleKey}
+            ref={(r) => ratingArrayRef.current?.push(r)}
+            role={isEditable ? 'slider' : ''}
+            aria-invalid={error ? true : false}
+            aria-valuenow={rating}
+            aria-valuemax={5}
+            aria-label={isEditable ? 'Укажите рейтинг' : 'рейтинг' + rating}
+            aria-valuemin={1}
           >
-            <StarIcon
-              tabIndex={isEditable ? 0 : -1}
-              onKeyDown={(e: KeyboardEvent<SVGElement>) =>
-                isEditable && handleSpace(i + 1, e)
-              }
-            />
+            <StarIcon />
           </span>
         )
       })
       setRatingArray(updatedArray)
     }
-    const changeDisplay = (rating: number) => {
+
+    const changeDispay = (i: number) => {
       if (!isEditable) {
         return
       }
-      constructRating(rating)
+      constructRating(i)
     }
-    const onClick = (rating: number) => {
+
+    const onClick = (i: number) => {
       if (!isEditable || !setRating) {
         return
       }
-      setRating(rating)
+      setRating(i)
     }
-    const handleSpace = (rating: number, event: KeyboardEvent<SVGElement>) => {
-      if (event.code !== 'Space' || !setRating) {
+
+    const handleKey = (e: KeyboardEvent) => {
+      if (!isEditable || !setRating) {
         return
       }
-      setRating(rating)
+      if (e.code == 'ArrowRight' || e.code == 'ArrowUp') {
+        if (!rating) {
+          setRating(1)
+        } else {
+          e.preventDefault()
+          setRating(rating < 5 ? rating + 1 : 5)
+        }
+        ratingArrayRef.current[rating]?.focus()
+      }
+      if (e.code == 'ArrowLeft' || e.code == 'ArrowDown') {
+        e.preventDefault()
+        setRating(rating > 1 ? rating - 1 : 1)
+        ratingArrayRef.current[rating - 2]?.focus()
+      }
     }
 
     return (
